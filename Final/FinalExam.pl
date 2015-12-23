a117053f6451a5e71db536a1043234db88a54e2b
Stewart <johnstons1@student.ncmich.edu>
2015-12-23 16:20:00 -0500

    Improved debugger. Added number input and validation functions. Added menu. Added Social Security Num input function.

--- Final/FinalExam.pl
+++ Final/FinalExam.pl
@@ -3,7 +3,7 @@
 ##Author: Stewart Johnston (Johnstons1@student.ncmich.edu)
 ##Assignment: Final Exam
 ##Purpose: Demonstrate mastery of perl as covered in class
-##Version: 0.2
+##Version: 0.3
 
 use 5.14.2;
 use warnings;
@@ -34,25 +34,36 @@ DBG_ARRAY => "List",
 DBG_IDX_ARRAY_LENGTH => 1,
 DBG_MDARRAY => "List of Lists",
 DBG_IDX_MDARRAY_DEPTH => 2,
+DBG_VARS => "Variables",
+};
+
+use constant {
+MENU_ONE => 1,
+MENU_EXIT => 0,
 };
 
 my @patientData;
 my @departmentCodes;
 
+my $continueInt;
+use constant USE_PROMPT => TRUE;
+
 #utils for my own shorthand.
 
 sub debugger {
 	my @localParms = @_;
-	if (DEBUG == TRUE && $localParms[0] =~ DBG_ARGS) {
+	if (DEBUG eq TRUE && $localParms[0] =~ DBG_ARGS || $localParms[0] =~ DBG_VARS) {
 		my @callerArgs = @localParms;
 		print "$callerArgs[0]:\n";
 		shift(@callerArgs);
-		foreach my $item (@callerArgs) {
-			print "$item\n";
+		if (scalar @callerArgs > 0) {
+			foreach my $item (@callerArgs) {
+				print "$item\n";
+			}
 		}
 		print "\n";
 	}
-	if (DEBUG == TRUE && $localParms[0] =~ DBG_ARRAY) {
+	if (DEBUG eq TRUE && $localParms[0] =~ DBG_ARRAY) {
 		my @callerElements = @localParms;
 		print "$callerElements[0]:\n";
 		my $callerArrayLength = 0;
@@ -87,6 +98,107 @@ sub ascBySSN {
 sub main {
 	populateDataArrays(); 
 	sortArrayByMthd(\@patientData,\&ascBySSN);
+	do {
+		menu();
+		setContinueInt(USE_PROMPT);
+	} while ($continueInt eq TRUE);
+}
+
+sub menu {
+	print "Search for patient records by Social Security Number (1), or exit (0)\n";
+	print "? ";
+	my $menuSelection = getNumInput(MENU_EXIT,MENU_ONE); 
+	if ($menuSelection eq MENU_ONE) {
+		print "Please enter Social Security Number for query: ";
+		my $querySSN = getSocialSecNum(); 
+		if ($querySSN eq 0) {
+			return 0;
+		}
+	}
+	elsif ($menuSelection eq MENU_EXIT) {
+		die "Exiting program.";
+	}
+}
+
+sub getSocialSecNum {
+	my $dirtyInput;
+	my $cleanInput;
+	my $inputValid = 0;
+	do {
+		chomp ($dirtyInput = <STDIN>);
+		debugger(DBG_VARS,"$dirtyInput");
+		if ($dirtyInput =~ /^[\d]{2,3}-?[\d]{0,2}-?[\d]{0,4}$/) {
+			$cleanInput = $dirtyInput;
+		} 
+		debugger(DBG_VARS,"$cleanInput","$dirtyInput");
+		if (defined $cleanInput && $cleanInput eq $dirtyInput) {
+			print "Input accepted.\n";
+			$inputValid = TRUE;
+		}
+		else {
+			print "Input rejected. Input either not a social security number or not properly formatted.\n";
+			print "Social Security Number format is nnn-nn-nnnn, where \"n\" is a digit.\n";
+			print "Partial Social Security Numbers also work, and one may omit the \"-\".\n";
+			$inputValid = 0;
+			setContinueInt(USE_PROMPT);
+		}
+	} until ($inputValid eq TRUE || $continueInt eq 0);
+	return $cleanInput; 
+}
+
+sub setContinueInt {
+	my $usePrompt = $_[0];
+	#my $prompt = $_[1];
+	$continueInt = -1;
+	if ($usePrompt eq USE_PROMPT) {
+	#	if (!defined $prompt) {
+			print "Do you want to continue? (" . TRUE . ":Yes 0:No): ";
+			$continueInt = getNumInput(0,TRUE);
+	#	}
+	#	elsif (defined $prompt) {
+	#		print "$prompt";
+	#	}
+	}
+	return $continueInt;
+}
+
+sub getNumInput {
+	my $rangeMin = $_[0];
+	my $rangeMax = $_[1];
+	my $dirtyInput;
+	my $cleanInput;
+	my $inputValid = 0;
+	do {
+		chomp ($dirtyInput = <STDIN>);
+		debugger(DBG_VARS,"$dirtyInput");
+		$cleanInput = validateNum($dirtyInput,$rangeMin,$rangeMax);
+		debugger(DBG_VARS,"$cleanInput,$dirtyInput");
+		if (defined $cleanInput && $cleanInput eq $dirtyInput) {
+			print "Input accepted.\n";
+			$inputValid = TRUE;
+		}
+		else {
+			print "Input rejected. Input either not a number or out of range. Range is $rangeMin - $rangeMax\n";
+			$inputValid = 0;
+			$cleanInput = setContinueInt(USE_PROMPT);
+		}
+	} until ($inputValid eq TRUE || $continueInt eq 0);
+	return $cleanInput;
+}
+	
+sub validateNum {
+	debugger(DBG_ARGS,@_);
+	my $dirtyInput = $_[0];
+	my $rangeMin = $_[1]; #these are inclusive
+	my $rangeMax = $_[2];
+	my $cleanInput;
+	if ($dirtyInput =~ /[\d]+/ && $dirtyInput !~ /[^\d]+/ && $rangeMin <= $dirtyInput && $dirtyInput <= $rangeMax) {
+		$cleanInput = $dirtyInput;
+	}
+	else {
+		$cleanInput = rand();
+	}
+	return $cleanInput;
 }
 
 sub populateDataArrays {
