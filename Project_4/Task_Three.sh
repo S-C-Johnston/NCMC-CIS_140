991b666d0397721087320bf4a74ea5013898c567
Stewart Johnston <johnstons1@student.ncmich.edu>
2016-10-24 16:39:50 -0400

    Updated boundaries in the regexes to match left and right word boundaries.
    The reason for this is that the original assumed knowledge about the format of the file and the order in which fields appeared. This only accounts for the format of the fields that need to be changed.

--- Project_4/Task_Three.sh
+++ Project_4/Task_Three.sh
@@ -5,8 +5,8 @@
 ## Version: 1.0
 ## Purpose: Demonstrate understanding of the stream editor
 
-sed 's/^([0-9]){3}-/(&) /' <PhoneList.csv >PhoneList.csv.new
-# Match at the beginning of the line 3 digits followed by -, capture the numbers, and wrap them in parens
-sed 's/\b([A-Za-z0-9._%+-])+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/\1@starhealth.org/'
+sed 's/\<([0-9]){3}-/(&) /' <PhoneList.csv >PhoneList.csv.new
+# Match at the boundary of a word 3 digits followed by -, capture the numbers, and wrap them in parens
+sed 's/\<([A-Za-z0-9._%+-])+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\>/\1@starhealth.org/'
 # www.regular-expressions.info/email.html
 # Repurposed for my needs, capturing the user's name and swapping out the old domain name
