8ee0024d2057d78fef19e3fda51e148cf84b1c42
Stewart Johnston <johnstons1@student.ncmich.edu>
2019-12-17 01:22:15 -0500

    Add sub to slurp unix config files
    For either "key = value" or "key [whitespace] value" formatted files, as
    often found in Unix systems.

--- project_8/project_8.pl
+++ project_8/project_8.pl
@@ -8,3 +8,24 @@ use v5.26.3;
 use warnings;
 use strict;
 
+sub read_config_file()
+{
+	my $file = shift 
+		or die "No file argument: $!\n";
+	my %configuration_hash;
+	open (CONFIG_FILE, '<', $file) 
+		or die "Can't open $file to read: $!\n";
+	while (<CONFIG_FILE>)
+	{
+		chomp (my $line = <CONFIG_FILE>);
+		$line =~ s/#.*//;
+		$line =~ s/^\s+//;
+		$line =~ s/\s+$//;
+		next unless (length($line) > 0);
+		my ($config_key, $config_value) 
+		= split (/\s*=?\s*/, $line);
+		$configuration_hash{$config_key} = $config_value;
+		#https://www.unix.com/shell-programming-and-scripting/161037-read-config-file-use-perl-program.html
+	}
+	return \%configuration_hash;
+}
