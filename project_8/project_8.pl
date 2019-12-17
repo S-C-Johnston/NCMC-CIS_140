22a1c6ac073236a8c5ceefd24455d41710a89e32
Stewart Johnston <johnstons1@student.ncmich.edu>
2019-12-17 00:45:27 -0500

    Initial commit on project 8

---
+++ project_8/project_8.pl
@@ -0,0 +1,10 @@
+## Name: Project 8
+## Purpose: Demonstrate an understanding of perl
+## Version: 2019.12.16.23
+## Author: Stewart Johnston
+## Email: johnstons1@student.ncmich.edu
+
+use v5.26.3;
+use warnings;
+use strict;
+
