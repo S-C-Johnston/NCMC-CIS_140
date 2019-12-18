53136b02e32b4df233212b20cdad621b207d7064
Stewart Johnston <johnstons1@student.ncmich.edu>
2019-12-18 09:18:59 -0500

    Add/flesh out system login definitions retrieval

--- project_8/project_8.pl
+++ project_8/project_8.pl
@@ -9,7 +9,7 @@ use warnings;
 use strict;
 use Carp;
 
-my %UID_LIMITS;
+my %SYS_LOGIN_DEFS;
 
 sub read_config_file
 {
@@ -35,4 +35,29 @@ sub read_config_file
 	}
 	close (CONFIG_FILE);
 	return \%configuration_hash;
+}
+
+sub set_sys_login_defs
+{
+	my $config_hash;
+	eval
+	{
+		$config_hash = read_config_file("/etc/login.defs");
+	}
+	$SYS_LOGIN_DEFS{UID_MIN} = defined ${$config_hash}{UID_MIN} ?
+	${$config_hash}{UID_MIN} : 1000;
+	$SYS_LOGIN_DEFS{UID_MAX} = defined ${$config_hash}{UID_MIN} ?
+	${$config_hash}{UID_MAX} : 60000;
+	$SYS_LOGIN_DEFS{SYS_UID_MIN} = defined
+	${$config_hash}{SYS_UID_MIN} ?
+	${$config_hash}{SYS_UID_MIN} : 201;
+	$SYS_LOGIN_DEFS{SYS_UID_MAX} = defined
+	${$config_hash}{SYS_UID_MIN} ?
+	${$config_hash}{SYS_UID_MAX} : 999;
+	if (defined ${$config_hash}{USERDEL_CMD})
+	{
+		$SYS_LOGIN_DEFS{USERDEL_CMD} =
+		${$config_hash}{USERDEL_CMD};
+		#single if here because there is no default.
+	}
 }
