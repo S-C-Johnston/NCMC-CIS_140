12949b1b477a3df19d95276fbd4704faa894be1a
Stewart Johnston <johnstons1@student.ncmich.edu>
2019-12-17 22:41:26 -0500

    Add safety checks and better exceptions

--- project_8/project_8.pl
+++ project_8/project_8.pl
@@ -7,14 +7,20 @@
 use v5.26.3;
 use warnings;
 use strict;
+use Carp;
+
+my %UID_LIMITS;
 
 sub read_config_file
 {
 	my $file = shift 
-		or die "No file argument: $!\n";
+		or croak ("No file argument: $!\n");
 	my %configuration_hash;
-	open (CONFIG_FILE, '<', $file) 
-		or die "Can't open $file to read: $!\n";
+	if (-r $file)
+	{
+		open (CONFIG_FILE, '<', $file) 
+			or croak ("Can't open $file to read: $!\n");
+	}
 	while (<CONFIG_FILE>)
 	{
 		chomp (my $line = <CONFIG_FILE>);
@@ -27,5 +33,6 @@ sub read_config_file
 		$configuration_hash{$config_key} = $config_value;
 		#https://www.unix.com/shell-programming-and-scripting/161037-read-config-file-use-perl-program.html
 	}
+	close (CONFIG_FILE);
 	return \%configuration_hash;
 }
